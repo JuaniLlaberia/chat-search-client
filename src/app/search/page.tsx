71e21a91f60a113650bf7efd674e16520caf1587
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-09-12 16:56:34 -0300

    Change pages structure

---
+++ src/app/search/page.tsx
@@ -0,0 +1,19 @@
+import InputBar from '@/components/custom/input-bar';
+import MessageArea from '@/components/custom/message-area';
+
+const SearchPage = () => {
+  return (
+    <section className='min-h-screen flex flex-col'>
+      <div className='flex-1 pb-24'>
+        <MessageArea />
+      </div>
+      <div className='fixed bottom-0 left-0 right-0 bg-background backdrop-blur-sm p-4 pt-0 z-10'>
+        <div className='w-full max-w-[50%] mx-auto'>
+          <InputBar includeSuggestions={false} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SearchPage;
